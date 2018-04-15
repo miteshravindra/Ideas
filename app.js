0958a0bf6065ee3675323dfb3657a5709f4aadd4
Mitesh <ravindramitesh@gmail.com>
2018-04-15 14:12:43 -0400

    able to add and remove data from mongodb database and added some new features

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine','handlebars');
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/ideas-app-dev')
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
 
+// Load idea Model
+require('./Models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Index Route
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -14,6 +31,54 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// Add Idea form
+
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+// Idea index page
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+        .sort({date: 'desc'})
+        .then(ideas => {
+            res.render('ideas/ideasPage', {
+                ideas: ideas
+            });
+        });
+});
+
+// Process form
+
+app.post('/ideas', (req, res) => {
+    let error = [];
+    if (!req.body.title || !req.body.details) {
+        if (!req.body.title) {
+            error.push({text: 'Title is required'});
+        }
+        if (!req.body.details) {
+            error.push({text: 'Detail is required'});
+        }
+        res.render('ideas/add', {
+            pageError: error,
+            title: req.body.title,
+            details: req.body.details
+        })
+    }
+    else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newUser)
+            .save().then((ideas) => {
+            res.redirect('/ideas');
+        })
+    }
+
+});
+
+
 app.listen(500, function () {
-    console.log('app is listening');
+    console.log('App is listening');
 });
